3135e88184cf48eae18c311457f6b33be823ccf6
Juliana_Almeida <julianagfalmeida@gmail.com>
2025-08-26 16:17:02 -0300

    Video added

--- src/sections/Media/Media.tsx
+++ src/sections/Media/Media.tsx
@@ -10,10 +10,24 @@ export const Media = component$(() => {
     style={{ boxShadow: 'inset -10px -700px 127.2px 58px rgba(255,255,255,0.24)', }}>
       
       <h1 class="text-white font-bold uppercase text-7xl text-center py-[10px]">MEDIA</h1>
-      {
-    <Carousel images={[feature1, feature2, feature3]} />
-    }
       
+    <Carousel images={[feature1, feature2, feature3]} />
+    
+    
+      
+    <div class="flex flex-wrap gap-2 justify-center overflow-x-auto px-2 py-10">
+      <iframe
+  width="1120"
+  height="630"
+  src="https://www.youtube.com/embed/-Q73MqCNeXs?si=HOvNP_MYv0BYTcIF"
+  title="YouTube video player"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+  referrerPolicy="strict-origin-when-cross-origin"
+></iframe>
+
+    </div>
+      
+      <h2 class="text-white font-bold uppercase text-7xl text-center py-[10px]">ASSISTA BONDIM ANIMADO</h2>
 
     </div>
   );
